c68cde4c5aae485b1fedcddb99d354556c2a920a
rodbastos <rodrigo@targetteal.com>
2024-11-28 11:34:52 -0300

    refactor: update deployment configuration to use gunicorn with uvicorn workers for better production setup

--- src/run_service.py
+++ src/run_service.py
@@ -6,4 +6,4 @@ from core import settings
 load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("service:app", host=settings.HOST, port=settings.PORT, reload=settings.is_dev())
+    uvicorn.run("service.service:app", host=settings.HOST, port=settings.PORT, reload=settings.is_dev())
